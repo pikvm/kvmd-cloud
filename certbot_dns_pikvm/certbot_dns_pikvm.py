ff4bd10c6345698719199560920bff1a78f5dd88
Maxim Devaev <mdevaev@gmail.com>
2023-01-16 22:26:25 +0200

    stub

--- certbot_dns_pikvm/certbot_dns_pikvm.py
+++ certbot_dns_pikvm/certbot_dns_pikvm.py
@@ -1,10 +1,15 @@
 import subprocess
 import logging
 
+from typing import Callable
+
 from certbot import errors
 from certbot.plugins import dns_common
 
-logger = logging.getLogger(__name__)
+
+# =====
+_logger = logging.getLogger(__name__)
+
 
 # =====
 class Authenticator(dns_common.DNSAuthenticator):
@@ -15,6 +20,11 @@ class Authenticator(dns_common.DNSAuthenticator):
     def more_info(self) -> str:
         return "This plugins configures a DNS TXT record to respond to a dns-01 challenge on PiKVM Cloud"
 
+    @classmethod
+    def add_parser_arguments(cls, add: Callable[..., None], default_propagation_seconds: int=10) -> None:
+        super().add_parser_arguments(add, default_propagation_seconds)
+        add("credentials", help='Stub')
+
     def _setup_credentials(self) -> None:
         pass
 
@@ -23,13 +33,13 @@ class Authenticator(dns_common.DNSAuthenticator):
         res = subprocess.run(["kvmd-cloudctl", "certbotAdd", combined_domain, validation], capture_output=True)
         if res.returncode != 0:
             raise errors.PluginError(f"Error adding TXT record: {res.stderr.decode()}")
-        logger.debug("Successfully added TXT record")
+        _logger.debug("Successfully added TXT record")
 
 
     def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
         combined_domain = f"{validation_name}.{domain}"
         res = subprocess.run(["kvmd-cloudctl", "certbotDel", combined_domain], capture_output=True)
         if res.returncode != 0:
-            logger.warning(f"Error deleting TXT record {combined_domain}")
+            _logger.warning(f"Error deleting TXT record {combined_domain}")
         else:
-            logger.debug("Successfully deleted TXT record")
+            _logger.debug("Successfully deleted TXT record")
