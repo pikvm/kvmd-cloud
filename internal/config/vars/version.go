e2a40e48b4ddf0ca8a955f6f01213224a82f3950
Vladimir Sukhonosov <founder.sl@gmail.com>
2025-05-28 22:21:47 +0100

    extend version string with release/debug flag

--- internal/config/vars/version.go
+++ internal/config/vars/version.go
@@ -2,17 +2,21 @@ package vars
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 var (
-	AppName       = ""
-	Version       = "dev"
-	Commit        = ""
-	CommitSHA     = ""
-	_build        = "debug"
-	Debug         = true
-	VersionString = ""
+	AppName         = ""
+	Version         = "dev"
+	Commit          = ""
+	CommitSHA       = ""
+	_buildTimestamp = ""
+	BuildTime       time.Time
+	_build          = "debug"
+	Debug           = true
+	VersionString   = ""
 )
 
 func init() {
@@ -20,10 +24,14 @@ func init() {
 		Debug = false
 	}
 
+	if ts, err := strconv.ParseInt(_buildTimestamp, 10, 64); err != nil {
+		BuildTime = time.Unix(ts, 0)
+	}
+
 	Version, _ = strings.CutPrefix(Version, "v")
 	if Commit == "" {
-		VersionString = fmt.Sprintf("%s version %s", AppName, Version)
+		VersionString = fmt.Sprintf("%s version %s [%s]", AppName, Version, _build)
 	} else {
-		VersionString = fmt.Sprintf("%s version %s [%s]", AppName, Version, Commit)
+		VersionString = fmt.Sprintf("%s version %s [%s] [%s]", AppName, Version, Commit, _build)
 	}
 }
